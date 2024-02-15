ce58fc3e490ffa96b40622940c048dba33b93774
Shalyca Sottoriva <ssottori@c3r3s3.42london.com>
2024-02-15 22:37:26 +0000

    norminettsabitch

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ssottori <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/11 20:57:04 by ssottori          #+#    #+#             */
-/*   Updated: 2024/02/14 19:39:22 by ssottori         ###   ########.fr       */
+/*   Updated: 2024/02/15 22:30:45 by ssottori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,31 +33,32 @@ typedef	struct checkData
 	bool	valid;
 }	checkData;
 
-bool	syntax_err(int *data);
-void	dup_err(int *data, int size);
-bool	stack_sorted(int *stack, int size);
-void	ft_error(char *type);
-void	fill_stack(int len, char **str, t_stack *stack, int i);
+bool		syntax_err(int *data);
+void		dup_err(int *data, int size);
+bool		stack_sorted(int *stack, int size);
+void		ft_error(char *type);
+void		fill_stack(int len, char **str, t_stack *stack, int i);
 checkData	input_check(t_stack *stack);
-t_stack	*init_stack(int ac, char **av);
-void	ft_exit(t_stack *stack);
-int	ft_superatoi(char *str);
+t_stack		*init_stack(int ac, char **av);
+void		ft_exit(t_stack *stack);
+int			ft_superatoi(char *str);
 
-int	sort_brain(t_stack *stack);
-void	smol_sort(t_stack *stack);
-int	sa(t_stack *stack);
-int	sb(t_stack *stack);
-int	ss(t_stack *stack);
+int			sort_brain(t_stack *stack);
+void		smol_sort(t_stack *stack);
 
-int	ra(t_stack *stack);
-int	rb(t_stack *stack);
-int	rr(t_stack *stack);
+int			sa(t_stack *stack);
+int			sb(t_stack *stack);
+int			ss(t_stack *stack);
 
-int	rra(t_stack *stack);
-int	rrb(t_stack *stack);
-int	rrr(t_stack *stack);
+int			ra(t_stack *stack);
+int			rb(t_stack *stack);
+int			rr(t_stack *stack);
 
-int	pa(t_stack *stack);
-int	pb(t_stack *stack);
+int			rra(t_stack *stack);
+int			rrb(t_stack *stack);
+int			rrr(t_stack *stack);
+
+int			pa(t_stack *stack);
+int			pb(t_stack *stack);
 
 #endif
