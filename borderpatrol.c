1c3005872b7f3422cb0f53e5851dd9e42581df9a
Shalyca Sottoriva <ssottori@c2r6s8.42london.com>
2024-03-07 23:17:25 +0000

    fuck push_swap

--- borderpatrol.c
+++ borderpatrol.c
@@ -6,7 +6,7 @@
 /*   By: ssottori <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 22:46:32 by ssottori          #+#    #+#             */
-/*   Updated: 2024/03/07 22:49:13 by ssottori         ###   ########.fr       */
+/*   Updated: 2024/03/07 23:14:06 by ssottori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,46 @@
 //Let them through border based of this and check for duplicates 
 //Bang bang
 
-void	size_matters
+#include "push_swap.h"
+
+void	signs(char *str)
+
+void	size_matters(long num, t_stack *stack)
+{
+	if (r > INT_MAX || r < INT_MIN)
+	{
+		free_matrix(str2);
+		ft_error("Out of Range!", stack);
+	}
+}
+
+int	ft_superatoi(char *str, char **str2, t_stack *stack)
+{
+	long			r;
+	int			s;
+	int			i;
+
+	r = 0;
+	s = 1;
+	i = 0;
+	while (ft_iswhitespace(str[i]))
+		i++;
+	if (str[i] == '+')
+		i++;
+	else if (str[i] == '-')
+	{
+		s = -1;
+		i++;
+	}
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+		{
+			free_matrix(str2);
+			ft_error("Non numeric character found!", stack);
+		}
+		r = r * 10 + str[i++] - '0';
+		size_matters(r, stack);
+	}
+	return (r * s);
+}
